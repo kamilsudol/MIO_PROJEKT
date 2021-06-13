d81cea4faf1bdb90adc90c442b404b810486fa5d
JJ807 <jakubstrugaa@gmail.com>
2021-06-13 22:11:26 +0200

    creating avi and gif out of frames

--- main.m
+++ main.m
@@ -64,6 +64,8 @@ function SSNvisualisation(layers, epochs)
     % obrobka i wyswietlanie danych
     x = linspace(0, epochs, epochs+1);
     
+    % wideo 1
+   
     % wykres jakosci nauczania sieci
     hold on
         figure(1)
@@ -71,6 +73,8 @@ function SSNvisualisation(layers, epochs)
         title("Performance MSE")
         xlabel('Liczba epok')
     hold off
+    
+
     
     % wykresy biasow
     plot_biases(biases, "Zmiana biasow w warstwie ", "Zmiana biasow na wyjsciu", 1, x);
@@ -104,6 +108,23 @@ function SSNvisualisation(layers, epochs)
 end
 
 function plot_biases(data, title_when_not_last, title_when_last, figure_move_parameter, x)
+
+    % video 1
+    framerate = 1; % predkosc odtwarzania klatek filmu
+    v = VideoWriter('bias_wyjscie.avi');
+    v.FrameRate = framerate; 
+    open(v);
+    
+    % video 2
+    v2 = VideoWriter('bias_warstwa.avi');
+    v2.FrameRate = framerate;
+    open(v2);
+    
+%   dzialaja jak flaga, potrzebne jest wstawienie pierwszego obrazka z
+%   innym parametrem w metodzie imwrite, niz dla reszty
+    counter4gif = 1;
+    counter4gif2 = 1;
+    
     for i=1:length(data(:,1))
         for j=1:length(cell2mat(data(i,1)))
             tmp = zeros(1, length(data(1,:)));
@@ -111,22 +132,64 @@ function plot_biases(data, title_when_not_last, title_when_last, figure_move_par
                 array_tmp = cell2mat(data(i, k));
                 tmp(k) = array_tmp(j);
             end
+           
             hold on
-            figure(i + figure_move_parameter)
+            axis tight manual % this ensures that getframe() returns a consistent size
+            filename1 = 'bias_wyjscie.gif';
+            filename2 = 'bias_warstwa.gif';
+            fig = figure(i + figure_move_parameter);
+            ylabel('Bias');
+            xlabel('Liczba epok');
+            
+            
             if i == length(data(:,1))
-                title(title_when_last)
+                title(title_when_last);
+                frame = getframe(fig);
+                im = frame2im(frame); 
+                [imind,cm] = rgb2ind(im,256);
+                if counter4gif == 1 
+                    imwrite(imind,cm,filename1,'gif', 'Loopcount',inf); 
+                    counter4gif = counter4gif + 1;
+                else 
+                    imwrite(imind,cm,filename1,'gif','WriteMode','append'); 
+                end 
+                writeVideo(v,frame);
             else
-                title(title_when_not_last + int2str(i))
+                title(title_when_not_last + int2str(i));
+                frame = getframe(fig);
+                
+                % gif
+                im = frame2im(frame); 
+                [imind,cm] = rgb2ind(im,256);
+                if counter4gif2 == 1 
+                    imwrite(imind,cm,filename2,'gif', 'Loopcount',inf);
+                    counter4gif2 = counter4gif2 + 1;
+                else 
+                    imwrite(imind,cm,filename2,'gif','WriteMode','append'); 
+                end 
+                
+                % vid
+                writeVideo(v2,frame);
             end
             plot(x, repmat(tmp,1));
-            xlabel('Liczba epok')
             hold off
+            
         end
     end
+    close(v);
+    close(v2);
 end
 
-
 function plot_first_weights(weights, figure_move_parameter, x, epochs)
+
+    % video
+    v = VideoWriter('wagi_wejscie.avi');
+    v.FrameRate = 1;
+    open(v);
+    
+    counter4gif = 1; % counter dla gifa
+    filename = 'wagi_wejscie.gif';
+    
     for i=1:length(weights(1,:,1))
         for j=1:length(weights(:,1,1))
             w_tmp = zeros(1, epochs+1);
@@ -134,17 +197,55 @@ function plot_first_weights(weights, figure_move_parameter, x, epochs)
                 w_tmp(k) = weights(j, i, k);
             end
             hold on
-            figure(figure_move_parameter + i)
-            title("Zmiana wag neuronow na wejsciu dla wejscia " + int2str(i))
-            plot(x, repmat(w_tmp,1));
-            xlabel('Liczba epok')
+                axis tight manual
+                fig = figure(figure_move_parameter + i);
+                title("Zmiana wag neuronow na wejsciu dla wejscia " + int2str(i))
+                plot(x, repmat(w_tmp,1));
+                ylabel('Waga');
+                xlabel('Liczba epok');
+                
+                frame = getframe(fig);
+                
+                % gif
+                im = frame2im(frame); 
+                [imind,cm] = rgb2ind(im,256);
+                if counter4gif == 1 
+                    imwrite(imind, cm, filename,'gif', 'Loopcount',inf);
+                    counter4gif = counter4gif + 1;
+                else 
+                    imwrite(imind, cm, filename,'gif','WriteMode','append'); 
+                end 
+                
+                % vid
+                writeVideo(v,frame);
             hold off
         end
     end
+    close(v);
 end
 
 
 function plot_layers(data, figure_move_parameter, x)
+
+    framerate = 1; % predkosc odtwarzania klatek filmu
+    
+    % video 1 
+    v1 = VideoWriter('wagi_wyjscie.avi');
+    v1.FrameRate = framerate;
+    open(v1);
+    
+    % video 2
+    v2 = VideoWriter('wagi_ukryta.avi');
+    v2.FrameRate = framerate;
+    open(v2);
+    
+    % countery dla gifow
+    counter4gif1 = 1;
+    counter4gif2 = 1;
+    
+    filename1 = 'wagi_wyjscie.gif';
+    filename2 = 'wagi_ukryta.gif';
+    
     plot_num = 0;
     for m=2:length(data(1,:,1))
         weights = cell2mat(data(m,m-1,:));
@@ -155,17 +256,51 @@ function plot_layers(data, figure_move_parameter, x)
                     w_tmp(k) = weights(j, i, k);
                 end
                 hold on
-                figure(figure_move_parameter + i + plot_num)
+                
+                axis tight manual
+                fig = figure(figure_move_parameter + i + plot_num);
+                ylabel('Waga');
+                xlabel('Liczba epok');
+                
                 if m == length(data(1,:,1))
                     title("Zmiana wag neuronow na wyjsciu dla wyjscia " + int2str(i))
+                    frame = getframe(fig);
+                    
+                    % gif
+                    im = frame2im(frame); 
+                    [imind,cm] = rgb2ind(im,256);
+                    if counter4gif1 == 1 
+                        imwrite(imind, cm, filename1,'gif', 'Loopcount',inf);
+                        counter4gif1 = counter4gif1 + 1;
+                    else 
+                        imwrite(imind, cm, filename1,'gif','WriteMode','append'); 
+                    end 
+                    
+                    % vid
+                    writeVideo(v1,frame);
                 else
                     title("Zmiana wag neuronow w warstwie ukrytej " + int2str(m-1) + " dla wyjscia " + int2str(i))
+                    frame = getframe(fig);
+                    
+                    % gif
+                    im = frame2im(frame); 
+                    [imind,cm] = rgb2ind(im,256);
+                    if counter4gif2 == 1 
+                        imwrite(imind, cm, filename2,'gif', 'Loopcount',inf);
+                        counter4gif2 = counter4gif2 + 1;
+                    else 
+                        imwrite(imind, cm, filename2,'gif','WriteMode','append'); 
+                    end 
+                    
+                    % vid
+                    writeVideo(v2,frame);
                 end
                 plot(x, repmat(w_tmp,1));
-                xlabel('Liczba epok')
                 hold off
             end
         end
         plot_num = plot_num + length(weights(1,:,1));
     end
+    close(v1);
+    close(v2);
 end
