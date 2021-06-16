a3ead74deaa7d07fefc626773cbdc234ffc20f4e
Kamil Sudoł <61806373+kamilsudol@users.noreply.github.com>
2021-06-16 11:12:41 +0200

    flags in main

--- main.m
+++ main.m
@@ -1,11 +1,7 @@
 clear; clc;
 
-SSNvisualisation(5, 50, 'compare');
+SSNvisualisation([2 2], 20, 'all');
 
-% SSNvisualisation(5, 50, 'animated');
+% SSNvisualisation([2 2], 20, 'compare');
 
-% SSNvisualisation(5, 50, 'animated');
-
-% SSNvisualisation([5 5 5 5 5 10 11 3], 20);
-
-% SSNvisualisation([1 2 3 4], 50);
+% SSNvisualisation([2 2], 20, 'animated');
